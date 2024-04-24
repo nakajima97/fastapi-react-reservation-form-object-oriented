d253cd3ae6a280634702be5b13fa26570ce5726b
nakajima97 <kituneudon97@gmail.com>
2024-04-25 08:56:37 +0900

    #1 使わないコードを削除

--- api/source/routers/holidays.py
+++ api/source/routers/holidays.py
@@ -3,16 +3,13 @@ from sqlalchemy.orm import Session
 
 from source.db import get_db
 from source.schemas.holidays import Holidays
-from source.cruds.calendars import store_holidays, fetch_holidays
-from source.authentication import get_api_key_from_header
 
 router = APIRouter()
 
 @router.get("/holidays", response_model=Holidays)
 async def get_holidays(db: Session = Depends(get_db)):
-  return await fetch_holidays(db)
+  return {"holidays": ['2024-05-10', '2024-05-11']}
 
 @router.post("/holidays", response_model=Holidays)
-async def post_holidays(holidays: Holidays, db: Session = Depends(get_db), api_key: str = Depends(get_api_key_from_header)):
-  calendar = await store_holidays(db, holidays)
-  return calendar
+async def post_holidays(holidays: Holidays, db: Session = Depends(get_db)):
+  return {"holidays": ['2024-05-10']}
