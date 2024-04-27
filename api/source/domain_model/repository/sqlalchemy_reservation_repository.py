aaab193b54fd597612b08c3bde7f1f86b00bd065
nakajima97 <kituneudon97@gmail.com>
2024-04-27 15:25:31 +0900

    #7 不要なimport文の削除

--- api/source/domain_model/repository/sqlalchemy_reservation_repository.py
+++ api/source/domain_model/repository/sqlalchemy_reservation_repository.py
@@ -1,6 +1,4 @@
 from sqlalchemy.orm import Session
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
 
 from source.models.reservations import Reservations as ReservationModel
 from source.domain_model.entity.reservation import Reservation
