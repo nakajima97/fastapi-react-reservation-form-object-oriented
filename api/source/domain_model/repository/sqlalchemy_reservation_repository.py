aadba70b5ae8c96d4f03a02c37f7c712f5d4d8d8
Nakajima <kituneudon97@gmail.com>
2024-04-27 16:20:58 +0900

    GET /reservationsの開発 (#10)

--- api/source/domain_model/repository/sqlalchemy_reservation_repository.py
+++ api/source/domain_model/repository/sqlalchemy_reservation_repository.py
@@ -1,4 +1,6 @@
+from typing import List
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import select
 
 from source.models.reservations import Reservations as ReservationModel
 from source.domain_model.entity.reservation import Reservation
@@ -16,3 +18,8 @@ class SqlAlchemyReservationRepository():
         await self.db.commit()
         await self.db.refresh(model)
         return model
+
+    async def fetch(self) -> List[ReservationModel]:
+        result = await self.db.execute(select(ReservationModel))
+        result_all = result.all()
+        return [reservation[0] for reservation in result_all]
--- api/source/routers/reservations.py
+++ api/source/routers/reservations.py
@@ -3,31 +3,39 @@ from sqlalchemy.orm import Session
 from source.db import get_db
 from source.schemas.reservations import Reservation as ReservationSchema, ResponseReservation, GetReservationResponse
 from source.domain_model.usecase.store_reservation import StoreReservation
+from source.domain_model.usecase.fetch_reservations import FetchReservations
 from source.domain_model.repository.sqlalchemy_reservation_repository import SqlAlchemyReservationRepository
 from source.domain_model.entity.reservation import Reservation
 
 router = APIRouter()
 
 @router.get("/reservations", response_model=GetReservationResponse)
-async def get_reservations():
-  return {
-      "reservations": [
-          {
-              "id": 1,
-              "date": "2024-04-13",
-              "name": "John Doe",
-              "email_address": "example@example.com",
-              "phone_number": "123-4567-8901",
-          },
-          {
-              "id": 2,
-              "date": "2024-04-30",
-              "name": "Deborah Doe",
-              "email_address": "example@example.com",
-              "phone_number": "123-4567-8901",
-          },
-      ]
-  }
+async def get_reservations(db: Session = Depends(get_db)):
+    sqlAlchemyReservationRepository = SqlAlchemyReservationRepository(db)
+    fetch_reservations = FetchReservations(sqlAlchemyReservationRepository)
+    result = await fetch_reservations.execute()
+    print(result)
+    print(type(result[0]))
+    print(dir(result[0]))
+    return {"reservations": result}
+    return {
+        "reservations": [
+            {
+                "id": 1,
+                "date": "2024-04-13",
+                "name": "John Doe",
+                "email_address": "example@example.com",
+                "phone_number": "123-4567-8901",
+            },
+            {
+                "id": 2,
+                "date": "2024-04-30",
+                "name": "Deborah Doe",
+                "email_address": "example@example.com",
+                "phone_number": "123-4567-8901",
+            },
+        ]
+    }
 
 @router.post("/reservations", response_model=ResponseReservation)
 async def create_reservation(reservation_schema: ReservationSchema, db: Session = Depends(get_db)):
