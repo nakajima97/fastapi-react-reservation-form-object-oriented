17802b62c52913e9969434cccc614aef9be521af
Yudai Nakajima <kituneudon97@gmail.com>
2024-04-28 16:52:59 +0900

    #19 本branchで作成すべきメソッドではなかったため削除

--- api/source/domain_model/repository/sqlalchemy_calendar_repository.py
+++ api/source/domain_model/repository/sqlalchemy_calendar_repository.py
@@ -7,13 +7,3 @@ from source.models.calendars import CalendarsModel
 class SqlAlchemyReservationRepository():
     def __init__(self, db: Session) -> None:
         self.db = db
-
-    def fetch_holidays(self):
-        result = db.execute(select(CalendarsModel.date).where(CalendarsModel.is_holiday == True))
-        result_all = result.all()
-
-        # 件数が0件の場合は空リストを返す
-        if len(result_all) == 0:
-            return []
-
-        return [raw[0] for raw in result_all]
