9a9db3ae6bb1c2e51f73425bc22af645fe07064e
Yudai Nakajima <kituneudon97@gmail.com>
2024-04-28 16:41:50 +0900

    #19 idクラスが抜けていたので追加

--- api/source/domain_model/entity/calendar.py
+++ api/source/domain_model/entity/calendar.py
@@ -1,8 +1,11 @@
 import datetime
 
+from source.domain_model.value_object.id import Id
+
 class Calendar():
     def __init__(
         self,
+        id: Id,
         date: datetime.date,
         is_holiday: bool
     ):
