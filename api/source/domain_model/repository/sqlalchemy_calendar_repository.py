e9011939e4288afe5560fc2cecca3a149e04208e
Nakajima <kituneudon97@gmail.com>
2024-04-28 19:09:43 +0900

    POST /holidays の開発 (#24)
    * #19 Calendarクラスを作成
    
    * #19 idクラスが抜けていたので追加
    
    * #19 calendar用のリポジトリを作成
    
    * #19 本branchで作成すべきメソッドではなかったため削除
    
    * #19 DBにデータを追加するメソッドを作成
    
    * #19 repositoryにimportに成功することを確認
    
    * #19 repositoryの実行時にエラーがでないことを確認
    
    * #19 テストコードからimportできることを確認
    
    * #19 repositoryのテストコードを作成
    
    * #19 DBに定休日を保存できるようにした

---
+++ api/source/domain_model/repository/sqlalchemy_calendar_repository.py
@@ -0,0 +1,21 @@
+from typing import List
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import select
+
+from source.models.calendars import Calendars as CalendarsModel
+from source.domain_model.entity.calendar import Calendar
+
+class SqlAlchemyCalendarRepository():
+    def __init__(self, db: Session) -> None:
+        self.db = db
+
+    def __toModel(self, calendar: Calendar) -> CalendarsModel:
+        return CalendarsModel(**calendar.toDict())
+
+    async def insert(self, calendars: List[Calendar]) -> None:
+        print(len(calendars))
+        for calendar in calendars:
+            calendar_model = self.__toModel(calendar)
+            print(calendar_model.date)
+            self.db.add(calendar_model)
+        await self.db.commit()
--- api/source/routers/holidays.py
+++ api/source/routers/holidays.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import Session
 
 from source.db import get_db
 from source.schemas.holidays import Holidays
+from source.domain_model.repository.sqlalchemy_calendar_repository import SqlAlchemyCalendarRepository
+from source.domain_model.entity.calendar import Calendar
+from source.domain_model.usecase.store_holidays import StoreHolidays
 
 router = APIRouter()
 
@@ -12,4 +15,11 @@ async def get_holidays(db: Session = Depends(get_db)):
 
 @router.post("/holidays", response_model=Holidays)
 async def post_holidays(holidays: Holidays, db: Session = Depends(get_db)):
+  calendar_repository = SqlAlchemyCalendarRepository(db)
+  store_holidays = StoreHolidays(calendar_repository)
+  calendars = []
+  for holiday in holidays.holidays:
+    calendar = Calendar(date=holiday, is_holiday=True)
+    calendars.append(calendar)
+  await store_holidays.execute(calendars)
   return {"holidays": ['2024-05-10', '2024-05-11']}
