17eac18fdf28e105a1f7746869888ad99ac425d5
Yudai Nakajima <kituneudon97@gmail.com>
2024-05-04 15:42:38 +0900

    #43 formatterの実行

--- api/source/domain_model/usecase/reservation/fetch_reservations.py
+++ api/source/domain_model/usecase/reservation/fetch_reservations.py
@@ -2,6 +2,7 @@ from source.domain_model.repository.sqlalchemy_reservation_repository import (
     SqlAlchemyReservationRepository,
 )
 
+
 class FetchReservations:
     def __init__(self, reservation_repository: SqlAlchemyReservationRepository):
         self.reservation_repository = reservation_repository
