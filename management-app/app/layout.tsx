cc0f98497e3b18d297d6889691a2cea10565eb30
Nakajima <kituneudon97@gmail.com>
2024-05-04 18:33:49 +0900

    管理画面を開発するための準備をした (#50)
    * #49 docker compose upでnext.jsの初期画面が表示されることを確認
    
    * #49 管理画面の初期設定を行った

---
+++ management-app/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "予約管理",
+  description: "予約を管理するアプリ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
