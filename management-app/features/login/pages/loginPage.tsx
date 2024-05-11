f30fc174a0e774742d1f7cfd72bfff4a91e9b1ed
Yudai Nakajima <kituneudon97@gmail.com>
2024-05-11 16:36:28 +0900

    #57 ログインページの静的バージョン作成

--- management-app/features/login/pages/loginPage.tsx
+++ management-app/features/login/pages/loginPage.tsx
@@ -1,5 +1,45 @@
+"use client";
+
+import { Box, TextField, Typography, Button } from "@mui/material";
+
 const loginPage = () => {
-  return <div>ログイページ</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        width: "100%",
+        maxWidth: "500px",
+        margin: "auto",
+        padding: "20px",
+        border: "1px solid #ccc",
+        borderRadius: "10px",
+      }}
+    >
+      <Typography variant="h4">ログイン</Typography>
+      <TextField
+        required
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        required
+        id="password"
+        label="Password"
+        name="password"
+        autoComplete="password"
+        type="password"
+        autoFocus
+      />
+      <Button type="submit" variant="contained" fullWidth>
+        ログイン
+      </Button>
+    </Box>
+  );
 };
 
 export default loginPage;
