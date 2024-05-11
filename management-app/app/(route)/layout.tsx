9198cdf97d85a01ff554cd1026e86d42b4a5fa92
Yudai Nakajima <kituneudon97@gmail.com>
2024-05-11 15:45:49 +0900

    #57 layout.tsxにある不要なコードを削除

--- management-app/app/(route)/layout.tsx
+++ management-app/app/(route)/layout.tsx
@@ -1,7 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "予約管理",
@@ -15,7 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
