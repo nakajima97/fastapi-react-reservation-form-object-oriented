a2d4104ddb5faa01b189545f78021cf90e49d867
Nakajima <kituneudon97@gmail.com>
2024-05-18 15:37:21 +0900

    storybookを導入した (#76)
    * #66 storybookの設定画面にアクセスできることを確認
    
    * #66 BaseLayoutをstorybookで表示

---
+++ management-app/components/BaseLayout/index.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BaseLayout from './index';
+
+const meta: Meta = {
+  title: 'BaseLayout',
+  component: BaseLayout,
+  parameters: {
+    nextjs: {
+      appDirectory: true
+    }
+  }
+};
+export default meta;
+
+export const Primary: StoryObj<typeof meta> = {
+  args: {
+    title: 'BaseLayout',
+  }
+}
