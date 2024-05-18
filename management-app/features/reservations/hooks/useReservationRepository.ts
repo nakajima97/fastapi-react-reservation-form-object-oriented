84493691816652d8274672a614b35f7552a58291
Yudai Nakajima <kituneudon97@gmail.com>
2024-05-18 17:30:49 +0900

    #77 関数の戻り値の型を追加

--- management-app/features/reservations/hooks/useReservationRepository.ts
+++ management-app/features/reservations/hooks/useReservationRepository.ts
@@ -1,7 +1,9 @@
 import camelcaseKeys from 'camelcase-keys';
 
+import { ReservationType } from '../types';
+
 const useReservationRepository = () => {
-  const fetchReservations = async () => {
+  const fetchReservations = async (): Promise<ReservationType[]> => {
     const response = await fetch('http://localhost:8000/reservations');
     const data = await response.json();
     return camelcaseKeys(data.reservations);
