5d1a18f26aece5403e553ea5539abcdba7c319ef
Nakajima <kituneudon97@gmail.com>
2024-05-23 08:57:59 +0900

    lintエラーの解消 (#86)
    * #85 formatterを実行
    
    * #85 使われていないimport文を削除

--- management-app/features/reservations/hooks/useReservationRepository.ts
+++ management-app/features/reservations/hooks/useReservationRepository.ts
@@ -1,11 +1,9 @@
-import camelcaseKeys from "camelcase-keys";
-
 import { ReservationType } from "../types";
 import { fetchData } from "../../../utils/fetch";
 
 const useReservationRepository = () => {
   const fetchReservations = async (): Promise<ReservationType[]> => {
-    return fetchData("http://localhost:8000/reservations")
+    return fetchData("http://localhost:8000/reservations");
   };
 
   return {
